fe3eb4c397d213d8143d4a5e5b417de789354f99
Wallace Leonhard <wallacelw22@gmail.com>
2022-05-25 21:45:04 -0300

    style: :art: Styling the new components created

---
+++ src/components/Aside/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  grid-area: AS;
+  background-color: #031927;
+  color: #fff;
+  border-right: 1px solid #28c2ff;
+`;
---
+++ src/components/MainHeader/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  grid-area: MH;
+  background-color: #031927;
+  color: #fff;
+  border-bottom: 1px solid #28c2ff;
+`;
