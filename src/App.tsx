0aa15334fad13375a6429fce828165d18b101eb4
Wallace Leonhard <wallacelw22@gmail.com>
2022-05-26 21:06:31 -0300

    style: :art: Setting the default theme on the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable react/function-component-definition */
 import React from "react";
+
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
+
 import Layout from "./components/Layout";
+import dark from "./styles/themes/dark";
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
 export default App;
