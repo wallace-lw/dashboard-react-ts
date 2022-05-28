307b58300484c4f41d26b2a4b422b54b3f8b4153
Wallace Leonhard <wallacelw22@gmail.com>
2022-05-27 21:23:56 -0300

    feat: :sparkles: Creating and Styling the MainHeader component

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable react/function-component-definition */
 import React from "react";
-import { Container } from "./styles";
+import { Container, Welcome, UserName, Profile } from "./styles";
 
 const MainHeader: React.FC = () => {
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toggle</h1>
+      <Profile>
+        <Welcome>
+          Bem-vindo,
+          <UserName> Wallace!</UserName>
+        </Welcome>
+      </Profile>
     </Container>
   );
 };
--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -3,6 +3,18 @@ import styled from "styled-components";
 export const Container = styled.div`
   grid-area: MH;
   background-color: ${props => props.theme.colors.primary};
-  color: ${props => props.theme.colors.white};
   border-bottom: 1px solid ${props => props.theme.colors.tertiary};
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
 `;
+
+export const Profile = styled.div`
+  color: ${props => props.theme.colors.white};
+`;
+
+export const Welcome = styled.h3``;
+
+export const UserName = styled.span``;
