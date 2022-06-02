5051cd4842674cff135af90949b03e5726f25440
Wallace Leonhard <wallacelw22@gmail.com>
2022-06-01 22:25:21 -0300

    feat: :sparkles: Creating a Menu for the nav

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,50 @@
 /* eslint-disable react/function-component-definition */
 import React from "react";
-import { Container } from "./styles";
+import {
+  AiFillDollarCircle,
+  AiOutlineArrowDown,
+  AiOutlineArrowUp,
+} from "react-icons/ai";
+import { MdDashboard } from "react-icons/md";
+import { BsBoxArrowInRight } from "react-icons/bs";
+
+import {
+  Container,
+  Header,
+  MenuContainer,
+  MenuItemLink,
+  IconDiv,
+  Heading,
+} from "./styles";
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <IconDiv>
+          <AiFillDollarCircle color="#F7931B" size="40px" />
+        </IconDiv>
+        <Heading>Minha Carteira</Heading>
+      </Header>
+
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <AiOutlineArrowDown />
+          Entradas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <AiOutlineArrowUp />
+          Saídas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <BsBoxArrowInRight />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 };
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -6,3 +6,51 @@ export const Container = styled.div`
   color: ${props => props.theme.colors.white};
   border-right: 1px solid ${props => props.theme.colors.tertiary};
 `;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 15px 10px 40px 10px;
+`;
+
+export const Heading = styled.h1`
+  color: ${props => props.theme.colors.tertiary};
+  font-size: 24px;
+`;
+
+export const IconDiv = styled.div`
+  padding-right: 5px;
+`;
+
+export const MenuContainer = styled.nav`
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MenuItemLink = styled.a`
+  text-decoration: none;
+  color: ${props => props.theme.colors.info};
+  font-weight: 500;
+  font-size: 1.2rem;
+  padding-bottom: 40px;
+  transition: all 0.3s;
+  display: flex;
+  align-items: center;
+
+  > svg {
+    font-size: 2rem;
+    color: ${props => props.theme.colors.tertiary};
+    padding-right: 10px;
+  }
+
+  &:hover {
+    color: ${props => props.theme.colors.tertiary};
+    text-decoration: underline;
+    opacity: 0.8;
+    > svg {
+      color: ${props => props.theme.colors.info};
+      transform: scale(1.2);
+    }
+  }
+`;
