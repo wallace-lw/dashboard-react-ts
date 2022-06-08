68e74898c71f16d37bd0097fc897ec6bf235a0e8
Wallace Leonhard <wallacelw22@gmail.com>
2022-06-07 21:19:37 -0300

    feat: :sparkles: Adding a new page to the application

---
+++ src/components/HistoryFinanceCard/styles.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+interface IContainerProps {
+  color: string;
+}
+
+interface ITagProps {
+  color: string;
+}
+
+export const Container = styled.li<IContainerProps>`
+  background-color: ${props => props.color};
+
+  list-style: none;
+
+  border-radius: 5px;
+
+  margin: 10px 0;
+  padding: 12px 10px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: relative;
+
+  transition: all 0.3s;
+  cursor: pointer;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    padding-left: 5px;
+    > span {
+      font-weight: 500;
+    }
+  }
+
+  &:hover {
+    box-shadow: 0 0 0.5em ${props => props.theme.colors.tertiary};
+    transform: scale(1.01);
+  }
+`;
+
+export const Tag = styled.div<ITagProps>`
+  position: absolute;
+  left: 3px;
+
+  width: 5px;
+  height: 60%;
+  border-radius: 5px;
+  background-color: ${props => props.color};
+`;
